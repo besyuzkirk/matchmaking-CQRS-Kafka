5a528b0876b64931cc9e09c58142a0a23f3f2739
besyuzkirk <ahmtk1910@gmail.com>
2023-10-09 00:08:25 +0300

    added-ocelot-apigateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,11 +1,19 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(settings => settings.WithDictionaryHandle());;
 
 var app = builder.Build();
 
@@ -14,6 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
@@ -22,4 +31,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await app.UseOcelot();
 app.Run();
